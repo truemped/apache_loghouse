63ae2c78ce6cc381a8e7e38ba8fa04292e23567a
Daniel Truemper <truemped@googlemail.com>
2009-07-26 16:30:38 +0200

    small bugfix in the decorator

--- log_house/decorators.py
+++ log_house/decorators.py
@@ -44,7 +44,7 @@ def ajax_required(f):
     """    
     def wrap(request, *args, **kwargs):
         if not request.is_ajax():
-            return HttpResponseBadRequest
+            return HttpResponseBadRequest()
         return f(request, *args, **kwargs)
     wrap.__doc__=f.__doc__
     wrap.__name__=f.__name__
