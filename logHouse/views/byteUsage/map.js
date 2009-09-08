773756d2db67cd63a5567a0169b0c9bc87fee885
Daniel Truemper <truemped@googlemail.com>
2009-09-08 15:01:15 +0200

    initial import of the view functions

---
+++ logHouse/views/byteUsage/map.js
@@ -0,0 +1,12 @@
+function(doc) {
+    // !code _attachments/lib/helper/parsedate.js
+    if( doc.bytes && doc.status_code && doc.status_code == 200 && doc.time ) {
+        var d = new Date();
+        d.setISO8601( doc.time );
+        emit( ['m', d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes()], doc.bytes );
+        emit( ['H', d.getFullYear(), d.getMonth(), d.getDate(), d.getHours()], doc.bytes );
+        emit( ['D', d.getFullYear(), d.getMonth(), d.getDate()], doc.bytes );
+        emit( ['M', d.getFullYear(), d.getMonth()], doc.bytes );
+        emit( ['Y', d.getFullYear()], doc.bytes );
+    }
+}
---
+++ logHouse/views/uris/map.js
@@ -0,0 +1,12 @@
+function(doc) {
+    // !code _attachments/lib/helper/parsedate.js
+    if( doc.uri && doc.status_code && doc.time ) {
+        var d = new Date();
+        d.setISO8601( doc.time );
+        emit( ['m', d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), doc.uri], 1 );
+        emit( ['H', d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), doc.uri], 1 );
+        emit( ['D', d.getFullYear(), d.getMonth(), d.getDate(), doc.uri], 1 );
+        emit( ['M', d.getFullYear(), d.getMonth(), doc.uri], 1 );
+        emit( ['Y', d.getFullYear(), doc.uri], 1 );
+    }
+}
