6fec34cce013572a83c5fc560d509f658fc63967
Daniel Truemper <truemped@googlemail.com>
2009-09-09 13:30:24 +0200

    analyzing the agent string searching for os, browser, bots and rss readers

--- scripts/loader.py
+++ scripts/loader.py
@@ -38,6 +38,8 @@ from couchdbkit.schema.properties import *
 from couchdbkit.session import create_session
 
 import datetime
+from httpagentparser import DetectorBase
+import httpagentparser
 
 month_map = {'Jan': 1, 'Feb': 2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 
         'Aug':8, 'Oct':9, 'Nov': 11, 'Dec': 12}
@@ -55,21 +57,44 @@ class Access(Document):
     time = DateTimeProperty()
     status_code = IntegerProperty()
     referral = StringProperty()
-    agent = StringProperty()
+    agentString = StringProperty()
     bytes = IntegerProperty()
 
 
 def parse(filename):
     'Return tuple of dictionaries containing file data.'
     def make_entry(x):
-        return Access ( ip = x.group('ip'),
-            uri = x.group('uri'),
-            time = apachetime( x.group('time') ),
-            status_code = int( x.group('status_code') ),
-            referral = x.group('referral'),
-            agent = x.group('agent'),
-            bytes = int( x.group('bytes') ),
-            )
+        a = Access ( ip = x.group('ip'),
+                uri = x.group('uri'),
+                time = apachetime( x.group('time') ),
+                status_code = int( x.group('status_code') ),
+                referral = x.group('referral'),
+                agentString = x.group('agent'),
+                bytes = int( x.group('bytes') ),
+                )
+
+        if x.group('agent') is not None:
+            agentInfo = httpagentparser.detect( x.group('agent') )
+            print agentInfo
+
+            if 'flavor' in agentInfo:
+                a['os'] = agentInfo['flavor'].keys()[0]
+                a['osVersion'] = agentInfo['flavor'][a['os']]['version']
+
+            if 'browser' in agentInfo:
+                a['browser'] = agentInfo['browser'].keys()[0]
+                a['browserVersion'] = agentInfo['browser'][a['browser']]['version']
+
+            if 'bot' in agentInfo:
+                a['bot'] = agentInfo['bot'].keys()[0]
+                a['botVersion'] = agentInfo['bot'][a['bot']]['version']
+
+            if 'rss_reader' in agentInfo:
+                a['rssReader'] = agentInfo['rss_reader'].keys()[0]
+                a['rssReaderVersion'] = agentInfo['rss_reader'][a['rssReader']]['version']
+
+        return a
+
     log_re = '(?P<ip>[.\d]+) - - \[(?P<time>.*?)\] "GET (?P<uri>.*?) HTTP/1.\d" (?P<status_code>\d+) (?P<bytes>\d+) "(?P<referral>.*?)" "(?P<agent>.*?)"'
     search = re.compile(log_re).search
     for line in open( filename, 'r'):
@@ -78,7 +103,37 @@ def parse(filename):
             yield make_entry(x)
 
 
+class Bot(DetectorBase):
+    info_type = "bot"
+    can_register = False
+
+class Google(Bot):
+    look_for = "Googlebot"
+
+class Netcraft(Bot):
+    look_for = "NetcraftSurveyAgent"
+
+class Yahoo(Bot):
+    look_for = "Yahoo! Slurp"
+
+class RssReader(DetectorBase):
+    info_type = "rss_reader"
+    can_register = False
+
+class AideRss(RssReader):
+    look_for = "AideRSS"
+
+class NetNewsWire(RssReader):
+    look_for = "NetNewsWire"
+
+
 def loadDb( uri, db, filename ):
+    httpagentparser.detectorshub.register( Google() )
+    httpagentparser.detectorshub.register( Netcraft() )
+    httpagentparser.detectorshub.register( Yahoo() )
+    httpagentparser.detectorshub.register( AideRss() )
+    httpagentparser.detectorshub.register( NetNewsWire() )
+
     s = Server( uri )
     database = s.get_or_create_db( db )
     session = create_session( s, db )
